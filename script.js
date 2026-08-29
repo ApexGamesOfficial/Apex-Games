1027485bb967a5ad7136ebb4d6244420852cf478
Apex Games <jfranke1230@gmail.com>
2026-08-29 18:48:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    document.querySelectorAll("a").forEach(link => {
+
+        link.addEventListener("click", () => {
+            document.body.classList.add("page-changing");
+        });
+
+    });
+
+});
